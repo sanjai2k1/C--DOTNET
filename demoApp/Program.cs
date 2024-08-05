f84321130e7eb92616a4b4bbb5e0cb4e7fc82417
sanjai2k1 <sanjai2k1j@gmail.com>
2024-08-05 14:02:56 +0530

    Add project files.

---
+++ demoApp/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello");
+        //System.Console.ReadKey();
+        //Console.Clear();
+        //Console.WriteLine("Welcome to .net class ");
+
+        //int num = 10;
+        //Console.WriteLine(num);
+
+        sbyte a = 10;
+        byte b = 20;
+        short c = 30;
+        ushort d = 40;
+
+        int e = 50;
+        uint f = 70;
+        long g = 55;
+        ulong h = 60;
+        float i = 90.23F;
+        double j = 100.234234;
+        decimal k = 110.2342432M;
+
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+        Console.WriteLine(g);
+        Console.WriteLine(h);
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(k);
+
+        int min = int.MinValue;
+        int max = int.MaxValue;
+        Console.WriteLine(min);
+        Console.WriteLine(max);
+
+
+
+
+
+    }
+}
